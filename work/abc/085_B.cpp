2675b24f16fa57e7f9ffe9906e2bbaa6e3f750c5
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-04-27 16:07:58 +0900

    fix abc 085 B

--- work/abc/085_B.cpp
+++ work/abc/085_B.cpp
@@ -1,25 +1,42 @@
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    vector<int> d(n);
 
+    set<int> values;
     for (int i = 0; i < n; i++) {
-        cin >> d.at(i);
+        int value;
+        cin >> value;
+        values.insert(value);
     }
 
-    sort(d.rbegin(), d.rend());
-
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (i == 0) {
-            count++;
-        } else if (d.at(i - 1) != d.at(i)) {
-            count++;
-        }
-    }
-
-    cout << count << endl;
+    cout << values.size() << endl;
 }
+
+// ver.1
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> d(n);
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> d.at(i);
+//     }
+
+//     sort(d.rbegin(), d.rend());
+
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         if (i == 0) {
+//             count++;
+//         } else if (d.at(i - 1) != d.at(i)) {
+//             count++;
+//         }
+//     }
+
+//     cout << count << endl;
+// }
