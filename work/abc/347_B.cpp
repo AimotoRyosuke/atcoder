fbc1b9ff85b16542ac06c3fa6efe05127eb7fe4d
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-04-30 11:09:13 +0900

    fix abc 347 B

--- work/abc/347_B.cpp
+++ work/abc/347_B.cpp
@@ -4,20 +4,38 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
-    vector<string> combinations;
+    set<string> combinations;
     for (int i = 0; i < s.size(); i++) {
         // iはsの文字列の開始位置
         for (int j = i; j < s.size(); j++) {
             // jはsの文字列の終了位置
             string sub = s.substr(i, j - i + 1);
-            cout << sub << endl;
-            // combinationsにsubが含まれていない場合、combinationsにsubを格納する
-            bool exist = find(combinations.begin(), combinations.end(), sub) !=
-                         combinations.end();
-            if (!exist) {
-                combinations.push_back(sub);
-            }
+            combinations.insert(sub);
         }
     }
     cout << combinations.size() << endl;
 }
+
+// ver.1
+// int main() {
+//     string s;
+//     cin >> s;
+//     vector<string> combinations;
+//     for (int i = 0; i < s.size(); i++) {
+//         // iはsの文字列の開始位置
+//         for (int j = i; j < s.size(); j++) {
+//             // jはsの文字列の終了位置
+//             string sub = s.substr(i, j - i + 1);
+//             cout << sub << endl;
+//             //
+//             combinationsにsubが含まれていない場合、combinationsにsubを格納する
+//             bool exist = find(combinations.begin(), combinations.end(), sub)
+//             !=
+//                          combinations.end();
+//             if (!exist) {
+//                 combinations.push_back(sub);
+//             }
+//         }
+//     }
+//     cout << combinations.size() << endl;
+// }
