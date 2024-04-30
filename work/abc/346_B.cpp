d39b6e3c4e4097d76012525e70721d214e0276cf
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-04-30 12:27:06 +0900

    fix 346_B

--- work/abc/346_B.cpp
+++ work/abc/346_B.cpp
@@ -1,4 +1,4 @@
-// 復讐する
+// 復習する
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -10,18 +10,17 @@ int main() {
     cin >> w >> b;
 
     for (int i = 0; i < (int)t.size(); i++) {
-        // nwはwの該当数、nbはbの該当数
-        int nw = 0, nb = 0;
+        int w_count = 0, b_count = 0;
         for (int j = 0; j < w + b; j++) {
             // i+jはtの文字列の開始位置
             // w+bは探索する文字列の最大長
             // もしtのi+j番目をtの長さで割った余りがwだったらnwをインクリメント、そうでなければnbをインクリメント
             if (t[(i + j) % t.size()] == 'w')
-                ++nw;
+                ++w_count;
             else
-                ++nb;
+                ++b_count;
         }
-        if (w == nw and b == nb) {
+        if (w_count == w && b_count == b) {
             cout << "Yes" << endl;
             return 0;
         }
