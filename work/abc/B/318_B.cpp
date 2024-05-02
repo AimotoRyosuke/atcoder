84d8dca285491cfdccc763e80db6285111ba3d7c
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-05-03 08:13:31 +0900

    abc 318 B ver.2

--- work/abc/B/318_B.cpp
+++ work/abc/B/318_B.cpp
@@ -4,15 +4,36 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    set<vector<int>> s;
+    map<int, set<int>> s;
     for (int i = 0; i < n; i++) {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
         for (int x = a + 1; x <= b; x++) {
             for (int y = c + 1; y <= d; y++) {
-                s.insert({x, y});
+                s[x].insert(y);
             }
         }
     }
-    cout << s.size() << endl;
+    int count = 0;
+    for (const auto &[key, value] : s) {
+        count += s.at(key).size();
+    }
+    cout << count << endl;
 }
+
+// ver.1
+// int main() {
+//     int n;
+//     cin >> n;
+//     set<vector<int>> s;
+//     for (int i = 0; i < n; i++) {
+//         int a, b, c, d;
+//         cin >> a >> b >> c >> d;
+//         for (int x = a + 1; x <= b; x++) {
+//             for (int y = c + 1; y <= d; y++) {
+//                 s.insert({x, y});
+//             }
+//         }
+//     }
+//     cout << s.size() << endl;
+// }
