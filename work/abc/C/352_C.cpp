21b4ca130fee60f879d6f5526702765c22bd74d3
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-05-05 09:05:08 +0900

    abc 352 C ver2

--- work/abc/C/352_C.cpp
+++ work/abc/C/352_C.cpp
@@ -5,22 +5,38 @@ using ll = long long;
 int main() {
     int N;
     cin >> N;
-    vector<vector<ll>> p(N, vector<ll>(2));
+    int max_diff = 0;
     ll height = 0;
+    int a, b;
     for (int i = 0; i < N; i++) {
-        cin >> p.at(i).at(0) >> p.at(i).at(1);
+        cin >> a >> b;
+        max_diff = max(max_diff, b - a);
+        height += a;
     }
-    sort(p.begin(), p.end(),
-         [](const vector<ll> &alpha, const vector<ll> &beta) {
-             return alpha[1] - alpha[0] < beta[1] - beta[0];
-         });
-    for (int i = 0; i < N; i++) {
-        if (i == N - 1) {
-            height += p.at(i).at(1);
-        } else {
-            height += p.at(i).at(0);
-        }
-    }
-
+    height += max_diff;
     cout << height << endl;
 }
+
+// ver.1
+// int main() {
+//     int N;
+//     cin >> N;
+//     vector<vector<ll>> p(N, vector<ll>(2));
+//     ll height = 0;
+//     for (int i = 0; i < N; i++) {
+//         cin >> p.at(i).at(0) >> p.at(i).at(1);
+//     }
+//     sort(p.begin(), p.end(),
+//          [](const vector<ll> &alpha, const vector<ll> &beta) {
+//              return alpha[1] - alpha[0] < beta[1] - beta[0];
+//          });
+//     for (int i = 0; i < N; i++) {
+//         if (i == N - 1) {
+//             height += p.at(i).at(1);
+//         } else {
+//             height += p.at(i).at(0);
+//         }
+//     }
+
+//     cout << height << endl;
+// }
