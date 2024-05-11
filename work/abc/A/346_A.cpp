b6b481c0f5c233f8e231eef4c805f1ed146660b6
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-05-11 20:17:26 +0900

    abc 346 A

---
+++ work/abc/A/346_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    for (int i = 0; i < N - 1; i++) {
+        cout << A.at(i) * A.at(i + 1) << " ";
+    }
+    cout << endl;
+}
