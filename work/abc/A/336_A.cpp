94f0d4e78b843b2029fdd0df93f7ff9d9328ee79
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-05-20 09:33:17 +0900

    abc 336 A

---
+++ work/abc/A/336_A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string s = "L";
+    for (int i = 0; i < N; i++) {
+        s += "o";
+    }
+    s += "ng";
+    cout << s << endl;
+}
