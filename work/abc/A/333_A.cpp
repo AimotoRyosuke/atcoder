8737766e00c66ccc9d0bd44886856d79877d492c
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-05-23 08:05:30 +0900

    abc 333 A

---
+++ work/abc/A/333_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string N_s = to_string(N);
+    for (int i = 0; i < N; i++) {
+        cout << N_s;
+    }
+    cout << endl;
+}
