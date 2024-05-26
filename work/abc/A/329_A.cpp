b9aa30cd8f9be0e3784e2d75bbf4186a9b0b26b8
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-05-27 07:28:01 +0900

    abc 329 A

---
+++ work/abc/A/329_A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    for (int i = 0; i < (int)S.size(); i++) {
+        cout << S.at(i) << " ";
+    }
+    cout << endl;
+}
