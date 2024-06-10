54d1c7e4cc3d4b0ca4c665c826113d1a8f392f20
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-06-11 08:26:38 +0900

    abc 314 A

---
+++ work/abc/A/314_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const string pi = "3."
+                  "141592653589793238462643383279502884197169399375105820974944"
+                  "5923078164062862089986280348253421170679";
+
+int main() {
+    int N;
+    cin >> N;
+    cout << pi.substr(0, N + 2) << endl;
+}
