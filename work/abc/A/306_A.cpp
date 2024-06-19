b86e390ff32cd32f5fec284bbb1cb7631750da16
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-06-20 06:56:48 +0900

    abc 306 A

---
+++ work/abc/A/306_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+    for (auto c : S)
+        cout << c << c;
+    cout << endl;
+}
