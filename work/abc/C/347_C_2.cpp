6d9a0c89d17a535b8b0276029e8f144d63e98b61
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-06-28 09:19:12 +0900

    abc 347 C 2回目

---
+++ work/abc/C/347_C_2.cpp
@@ -0,0 +1,32 @@
+// 復習する
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int N, A, B;
+    cin >> N >> A >> B;
+    int W = A + B;
+    vector<int> D(N);
+    for (auto &d : D) {
+        cin >> d;
+        d % W;
+    }
+    sort(D.begin(), D.end());
+    rep(i, N) D.push_back(D.at(i) + W);
+    bool ok = false;
+    rep(i, D.size() - 1) {
+        if ((D[i + 1] - D[i] >= B + 1)) {
+            ok = true;
+            break;
+        }
+    }
+    rep(i, N) {
+        int l = D.at(i), r = D.at(i + N - 1);
+        if (r - l + 1 <= A) {
+            ok = true;
+            break;
+        }
+    }
+    cout << (ok ? "Yes" : "No") << endl;
+}
