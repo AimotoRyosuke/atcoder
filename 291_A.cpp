3dd0eecdfc992f2bec56d9a4296e623ef5a1e870
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-07-06 08:47:06 +0900

    abc 291 A

---
+++ 291_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    string S;
+    cin >> S;
+    rep(i, S.size()) {
+        if (S.at(i) - 'A' > 0 && S.at(i) - 'A' < 26) {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+}
