09e2cd55915b4096303b1b2acbccdcaf40f46352
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-07-14 13:26:54 +0900

    abc 319 C 2回目

---
+++ work/abc/C/319_C_2.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    vector<int> c(9);
+    rep(i, 9) cin >> c.at(i);
+    vector<int> p(9);
+    iota(p.begin(), p.end(), 0);
+    int cnt = 0, tot = 0;
+    do {
+        tot++;
+        bool ok = true;
+        auto f = [&](int i, int j, int k) {
+            vector<pair<int, int>> d;
+            d.emplace_back(p.at(i), c.at(i));
+            d.emplace_back(p.at(j), c.at(j));
+            d.emplace_back(p.at(k), c.at(k));
+            sort(d.begin(), d.end());
+            if (d[0].second == d[1].second)
+                ok = false;
+        };
+        f(0, 1, 2);
+        f(3, 4, 5);
+        f(6, 7, 8);
+        f(0, 3, 6);
+        f(1, 4, 7);
+        f(2, 5, 8);
+        f(0, 4, 8);
+        f(2, 4, 6);
+        if (ok)
+            cnt++;
+
+    } while (next_permutation(p.begin(), p.end()));
+    double ans = 1. * cnt / tot;
+    printf("%.12f\n", ans);
+}
+
+// int main() {
+//     vector<int> c(9);
+//     rep(i, 9) { cin >> c.at(i); }
+//     vector<int> p;
+//     iota(p.begin(), p.end(), 0);
+//     int cnt = 0, tot = 0;
+//     do {
+//         bool ok = true;
+//         auto f = [&](int i, int j, int k) {
+//             vector<pair<int, int>> d;
+//             d.emplace_back(p[i], c[i]);
+//             d.emplace_back(p[j], c[j]);
+//             d.emplace_back(p[k], c[k]);
+//             sort(d.begin(), d.end());
+//             if (d[0].second == d[1].second)
+//                 ok = false;
+//         };
+//         f(0, 1, 2);
+//         f(3, 4, 5);
+//         f(6, 7, 8);
+//         f(0, 3, 6);
+//         f(1, 4, 7);
+//         f(2, 5, 8);
+//         f(0, 4, 8);
+//         f(2, 4, 6);
+//         if (ok)
+//             cnt++;
+//         tot++;
+
+//     } while (next_permutation(p.begin(), p.end()));
+//     double ans = 1. * cnt / tot;
+//     printf("%.10f\n", ans);
+// }
