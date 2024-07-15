6a1eed9c5893eb4c16ae40611b589677b2fd4d76
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-07-16 08:31:43 +0900

    abc 281 A

---
+++ work/abc/A/281_A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int N;
+    cin >> N;
+    rep(i, N + 1) { cout << N - i << '\n'; }
+}
