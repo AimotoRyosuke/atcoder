7243807712cd74169debd7ada5ad8ea3f962ed56
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-07-24 06:38:08 +0900

    abc 274 A

---
+++ work/abc/A/274_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    double ans = 1. * b / a;
+    printf("%.3f\n", ans);
+}
