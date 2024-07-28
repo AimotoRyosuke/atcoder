e0c1eea64109e60d5355bbce18b984e4a427ca2e
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-07-29 08:29:58 +0900

    abc 269 A

---
+++ work/abc/A/269_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c - d) << '\n';
+    cout << "Takahashi" << '\n';
+}
