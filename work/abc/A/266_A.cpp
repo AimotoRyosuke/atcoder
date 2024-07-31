014b8cb2e9da71dac3d6d07313e6d6ff0a751068
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-01 07:47:25 +0900

    abc 266 A

---
+++ work/abc/A/266_A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s[(s.size() + 1) / 2 - 1] << '\n';
+}
