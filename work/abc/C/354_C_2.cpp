bb2ac23eea14d35e1ef1a5bb2537209654d3741b
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-03 17:00:34 +0900

    abc 354 C 2回目

--- work/abc/C/354_C_2.cpp
+++ work/abc/C/354_C_2.cpp
@@ -2,38 +2,40 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
-struct Card {
-    int a, c, i;
-};
-
 int main() {
     int N;
     cin >> N;
-    vector<Card> cards(N);
-    rep(i, N) {
-        Card card;
-        cin >> card.a >> card.c;
-        card.i = i;
-        cards[i] = card;
-    }
+    vector<pair<int, int>> cards(N);
+    for (auto &card : cards)
+        cin >> card.first >> card.second;
 
-    sort(cards.begin(), cards.end(), [](const auto &a, const auto &b) {
-        if (a.c != b.c) {
-            return a.c < b.c;
+    auto A = cards;
+    sort(A.begin(), A.end(), [](const auto &a, const auto &b) {
+        if (a.first != b.first) {
+            return a.first > b.first;
         }
-        return a.a > b.a;
+        return a.second < b.second;
     });
-    int strength = 0;
-    vector<int> ans;
-    rep(i, N) {
-        if (strength <= cards[i].a) {
-            strength = cards[i].a;
-            ans.push_back(cards[i].i);
-            continue;
+    int l = 0;
+    map<pair<int, int>, int> w;
+    int next = 0;
+    rep(r, N) {
+        auto cl = A.at(l), cr = A.at(r);
+        if (r != 0 && cr.first != A.at(r - 1).first && cl.second > cr.second) {
+            if (next) {
+                l = next;
+                cl = A.at(l), cr = A.at(r);
+            }
+            next = r;
+        }
+        if (cl.first > cr.first && cl.second < cr.second) {
+            w[cr] = 1;
         }
     }
-    sort(ans.begin(), ans.end());
-    int size = ans.size();
-    cout << size << '\n';
-    rep(i, size) { cout << ans.at(i) + 1 << ' '; }
+    cout << N - w.size() << '\n';
+    rep(i, N) {
+        if (w[cards[i]])
+            continue;
+        cout << i + 1 << " ";
+    }
 }
