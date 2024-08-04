896ef6d3ae140930dad29740e6895fd7f75d69b2
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-04 11:02:53 +0900

    abc 361 D 2回目

--- work/abc/D/361_D_2.cpp
+++ work/abc/D/361_D_2.cpp
@@ -2,49 +2,42 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
-struct State {
-    string stones;
-    int cnt;
-};
-
-int minMoves(string start, string goal) {
-    queue<State> q;
-    q.push({start, 0});
-
-    unordered_set<string> visited;
-    visited.insert(start);
-    while (!q.empty()) {
-        State s = q.front();
-        q.pop();
-        int j = 0;
-        while (s.stones.at(j) != '.')
-            j++;
-
-        if (s.stones == goal)
-            return s.cnt;
-
-        rep(i, start.size() - 1) {
-            if (i == j)
-                continue;
-            auto next = s.stones;
-            swap(next.at(i), next.at(j));
-            swap(next.at(i + 1), next.at(j + 1));
-            if (visited.find(next) != visited.end())
-                continue;
-            visited.insert(next);
-            q.push({next, s.cnt + 1});
-        }
-    }
-    return -1;
-}
-
 int main() {
     int N;
     cin >> N;
-    string S, T;
-    cin >> S >> T;
-    int result = minMoves(S + "..", T + "..");
-    cout << result << endl;
+    string s, t;
+    cin >> s >> t;
 
-    return 0;
+    auto f = [&]() -> int {
+        auto start = s + "..";
+        auto tgt = t + "..";
+        unordered_map<string, bool> used;
+        queue<pair<string, int>> que;
+        que.emplace(start, 0);
+        used[start] = true;
+        while (que.size()) {
+            pair<string, int> q = que.front();
+            que.pop();
+            if (q.first == tgt) {
+                return q.second;
+            }
+            int j = 0;
+            while (q.first.at(j) != '.')
+                j++;
+            rep(i, N + 1) {
+                if (q.first.at(i) == '.' || q.first.at(i + 1) == '.')
+                    continue;
+                auto next = q.first;
+                swap(next.at(i), next.at(j));
+                swap(next.at(i + 1), next.at(j + 1));
+                if (used[next])
+                    continue;
+                used[next] = true;
+                que.emplace(next, q.second + 1);
+            }
+        }
+        return -1;
+    };
+    int ans = f();
+    cout << ans << '\n';
 }
