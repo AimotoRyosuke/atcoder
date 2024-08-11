592ef0e1516d5f8cf4673d6a63a44ce515ff09a5
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-12 08:39:46 +0900

    abc 256 A

---
+++ work/abc/A/256_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ull = unsigned long long;
+
+int main() {
+    int N;
+    cin >> N;
+    cout << (ull)pow(2, N) << '\n';
+}
