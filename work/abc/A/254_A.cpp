bf3dd1c6d1cee729d1da02a1e63e3ecec6ae3272
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-13 07:40:25 +0900

    abc 254 A

---
+++ work/abc/A/254_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int N;
+    cin >> N;
+    int ans = N % 100;
+    printf("%02d\n", ans);
+}
