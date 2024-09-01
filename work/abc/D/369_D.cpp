2d59bd02c9564628d934e6c9cd0ec9c68bdea87b
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-09-01 09:04:53 +0900

    abc 269 D

---
+++ work/abc/D/369_D.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+    ll INF = 1e18;
+    vector<vector<ll>> dp(N + 1, vector<ll>(2, -INF));
+    dp[0][0] = 0;
+    rep(i, N) {
+        int ni = i + 1;
+        cout << "here" << '\n';
+        rep(j, 2) {
+            {
+                int nj = j ^ 1;
+                int x = j % 2 ? A.at(i) * 2 : A.at(i);
+                dp[ni][nj] = max(dp[ni][nj], dp[i][j] + x);
+            }
+            {
+                int nj = j;
+                dp[ni][nj] = max(dp[ni][nj], dp[i][j]);
+            }
+        }
+    }
+    rep(i, N + 1) { cout << dp[i][0] << " " << dp[i][1] << '\n'; }
+    ll ans = max(dp[N][0], dp[N][1]);
+    cout << ans << '\n';
+}
+
+// int main() {
+//     int N;
+//     cin >> N;
+//     vector<int> A(N);
+//     for (auto &a : A)
+//         cin >> a;
+//     const ll INF = 1e18;
+//     vector<vector<ll>> dp(N + 1, vector<ll>(2, -INF));
+//     rep(i, N) {
+//         int ni = i + 1;
+//         rep(j, 2) {
+//             {
+//                 int nj = j ^ 1;
+//                 int X = j % 2 ? A.at(i) * 2 : A.at(i);
+//                 dp[ni][nj] = max(dp[ni][nj], dp[ni][nj] + X);
+//             }
+//             {
+//                 int nj = j;
+//                 dp[ni][nj] = max(dp[ni][nj], dp[i][j]);
+//             }
+//         }
+//     }
+//     ll ans = max(dp[N][0], dp[N][1]);
+//     cout << ans << '\n';
+// }
+
+// void chmax(ll &x, ll y) { x = max(x, y); }
+
+// int main() {
+//     int N;
+//     cin >> N;
+//     vector<int> A(N);
+//     for (auto &a : A)
+//         cin >> a;
+//     const ll INF = 1e18;
+//     vector<vector<ll>> dp(N + 1, vector<ll>(2, -INF));
+//     dp[0][0] = 0;
+//     rep(i, N) {
+//         int ni = i + 1;
+//         rep(j, 2) {
+//             {
+//                 int nj = j ^ 1;
+//                 int X = (j % 2) ? A[i] * 2 : A[i];
+//                 chmax(dp[ni][nj], dp[i][j] + X);
+//             }
+//             {
+//                 int nj = j;
+//                 chmax(dp[ni][nj], dp[i][j]);
+//             }
+//         }
+//     }
+//     ll ans = max(dp[N][0], dp[N][1]);
+//     cout << ans << '\n';
+// }
