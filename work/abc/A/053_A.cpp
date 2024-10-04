96f4a76e67080623cb641fa44ca00405dcf7364b
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-10-04 23:07:52 +0900

    abc 053 A

---
+++ work/abc/A/053_A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int N;
+    cin >> N;
+    cout << (N >= 1200 ? "ARC" : "ABC") << '\n';
+}
