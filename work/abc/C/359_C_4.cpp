1bb48eb88485c6709b56cd6ac1f1d984c57fca65
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-10-08 08:01:17 +0900

    abc 359 C 4回目

---
+++ work/abc/C/359_C_4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    ll sx, sy, tx, ty;
+    cin >> sx >> sy >> tx >> ty;
+    if ((sx + sy) % 2 == 0)
+        sx++;
+    if ((tx + ty) % 2 == 0)
+        tx++;
+    ll y = abs(ty - sy);
+    ll x = max(0LL, (abs(tx - sx) - y) / 2);
+    cout << x + y << '\n';
+}
